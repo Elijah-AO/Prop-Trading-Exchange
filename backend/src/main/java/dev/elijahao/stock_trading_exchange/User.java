db0154b0782e4c1815d288d7add12eaf1286b2be
Elijah <eolokun1806@gmail.com>
2024-08-01 23:08:31 +0100

    Created user class

---
+++ backend/src/main/java/dev/elijahao/stock_trading_exchange/User.java
@@ -0,0 +1,58 @@
+package dev.elijahao.stock_trading_exchange;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
