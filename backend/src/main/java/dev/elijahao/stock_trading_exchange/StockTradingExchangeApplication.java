86ff2aae0d02355bebbfcbbfb51352425121b64a
Elijah <eolokun1806@gmail.com>
2024-08-01 22:34:23 +0100

    Added springboot boilerplate

---
+++ backend/src/main/java/dev/elijahao/stock_trading_exchange/StockTradingExchangeApplication.java
@@ -0,0 +1,13 @@
+package dev.elijahao.stock_trading_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingExchangeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/elijahao/stock_trading_exchange/StockTradingExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elijahao.stock_trading_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
